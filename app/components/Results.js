a21cb21168a918590ee66541a083529504cda8b8
EDWARD <edsztukowski@gmail.com>
2017-06-16 18:12:09 -0600

    Adding state for results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -3,16 +3,52 @@ var queryString = require('query-string');
 var api = require('../utils/api')
 
 class Results extends React.Component {
+  construction(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+
+
   componentDidMount() {
     var players = queryString.parse(this.props.location.search)
     api.battle([
       players.playerOneName,
       players.playerTwoName
     ]).then(function(results) {
-      console.log(results)
-    })
+      if (results === null) {
+        return this.setState(function() {
+          return {
+            error: 'Looks like there was an error. Make sure both users exist on github',
+            loading: false
+          }
+        });
+      }
+      this.setState(function() {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      });
+    }.bind(this));
   }
   render() {
+    var error = this.state.error;
+    var winner = this.state.winnder;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading</p>
+    }
+
     return (
       <div>Results</div>
     )
