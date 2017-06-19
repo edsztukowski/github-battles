999cffd71011b11cfacdbfc0645720641035cc91
EDWARD <edsztukowski@gmail.com>
2017-06-19 17:43:11 -0600

    Adding loading component to results and popular

---
+++ app/components/Loading.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+}
+
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: props.text
+    };
+  }
+  componentDidMount() {
+    var stopper = this.props.text + '...';
+    this.interval = window.setInterval(function() {
+      
+    }, 300)
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    )
+  }
+}
+
+Loading.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+Loading.defaultProps = {
+  text: 'Loading'
+}
+
+module.exports = Loading;
